4314d253b886ec3ba2a3af85cc20414b9711dfec
fabien <fabien.bidault.pro@gmail.com>
2020-07-23 15:26:08 +0200

    creation category controller

--- src/main/java/fr/greta/filrouge/controller/CategorieController.java
+++ src/main/java/fr/greta/filrouge/controller/CategorieController.java
@@ -1,10 +1,31 @@
 package fr.greta.filrouge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+
+/*Categorie
+show "/categorie/{id}"
+showAll "/categorie/"
+add "/produit/categorie/add"
+update "/produit/categorie/update"
+delete "/produit/categorie/delete"*/
 
 @Controller
 public class CategorieController {
+	@GetMapping("/category/{id}")
+	public ModelAndView show(ModelAndView mv, @PathVariable int id) {
+		mv.setViewName("/category/show");
+	return mv;
+	}
+
+	@GetMapping("/category")
+	public ModelAndView showAll(ModelAndView mv) {
+		mv.setViewName("/category/showAll");
+	return mv;
+	}
 	
 
 }
