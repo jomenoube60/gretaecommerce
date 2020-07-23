cbcbd017cc05b59cfc831d2ae54a05e6dfb21787
fabien <fabien.bidault.pro@gmail.com>
2020-07-23 14:12:29 +0200

    creation produit contrrolleur

--- src/main/java/fr/greta/filrouge/controller/CategorieController.java
+++ src/main/java/fr/greta/filrouge/controller/CategorieController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class CatalogueController {
+public class CategorieController {
 	
 
 }
