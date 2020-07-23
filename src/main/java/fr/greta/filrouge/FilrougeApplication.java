e8cf5b001b420f5fc93dac164ffe500f179f9bb7
jomenoube60 <jo.menoube@gmail.com>
2020-07-23 09:04:37 +0200

    premier commi

---
+++ src/main/java/fr/greta/filrouge/FilrougeApplication.java
@@ -0,0 +1,13 @@
+package fr.greta.filrouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilrougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilrougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/greta/filrouge/FilrougeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.greta.filrouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilrougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
