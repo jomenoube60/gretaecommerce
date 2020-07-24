a1a601672d4d0f5eb399c5b12115698939dff70f
jomenoube60 <jo.menoube@gmail.com>
2020-07-24 22:48:35 +0200

    update Menu Model

--- src/main/java/fr/greta/filrouge/model/Menu.java
+++ src/main/java/fr/greta/filrouge/model/Menu.java
@@ -1,5 +1,32 @@
 package fr.greta.filrouge.model;
 
-public class Menu {
+import java.io.Serializable;
+
+public class Menu implements Serializable {
+	
+	private double prixHT;
+	private String name;
+	private boolean disponible;
+	
+	public double getPrixHT() {
+		return prixHT;
+	}
+	public void setPrixHT(double prixHT) {
+		this.prixHT = prixHT;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isDisponible() {
+		return disponible;
+	}
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	
+	
 	
 }
