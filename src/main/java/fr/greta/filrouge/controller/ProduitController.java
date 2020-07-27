3b732b0b44d673ebd980b5b29c1034da570d6fff
jomenoube60 <jo.menoube@gmail.com>
2020-07-27 09:45:16 +0200

    ajout controlleur produit

--- src/main/java/fr/greta/filrouge/controller/ProduitController.java
+++ src/main/java/fr/greta/filrouge/controller/ProduitController.java
@@ -1,57 +1,77 @@
 package fr.greta.filrouge.controller;
-
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import fr.greta.filrouge.model.Produit;
+
+
+
 public class ProduitController {
-
-	@GetMapping("/produit/{id}")
-	public ModelAndView show(ModelAndView mv, @PathVariable int id) {
-		mv.setViewName("/produit/show");
-	return mv;
-	}
-
 	@GetMapping("/produit")
-	public ModelAndView showAll(ModelAndView mv) {
-		mv.setViewName("/produit/showAll");
-	return mv;
+	public ModelAndView afficherLivres(ModelAndView mv) {
+		ArrayList<String> products = new ArrayList<>();
+		products.add("produit 1");
+		products.add("produit 2");
+		products.add("produit 3");
+		products.add("produit 4");
+
+
+		mv.addObject("isRestaurateur", true);
+		mv.addObject("products", products);
+
+		mv.setViewName("produit/showAll");
+		return mv;
 	}
-	
-	@GetMapping("/produit/restaurateur/add")
-	public ModelAndView addForm(ModelAndView mv) {
-		mv.setViewName("/produit/addForm");
-	return mv;
+	@GetMapping("/restaurateur/produit/add")
+	public ModelAndView afficherAddForm(ModelAndView mv) {
+		Produit product = new Produit();
+		mv.addObject("product", product);
+		mv.setViewName("produit/ajoutForm");
+		return mv;
 	}
-	
-	@PostMapping("/produit/restaurateur/add")
-	public ModelAndView treatAddForm(ModelAndView mv) {
-		mv.setViewName("/produit/addForm");
-	return mv;
+	@PostMapping("/restaurateur/produit/add")
+	public ModelAndView traiterAddform(ModelAndView mv, @Valid Produit product, BindingResult errors) {
+		if(!errors.hasErrors()) {
+			mv.setViewName("redirect:/produit");
+		}
+		else {
+			mv.setViewName("produit/ajoutForm");
+		}
+		return mv;
 	}
-	
-	@GetMapping("/produit/restaurateur/update/{id}")
-	public ModelAndView treatUpdateForm(ModelAndView mv) {
-		mv.setViewName("/produit/updateForm");
-	return mv;
+
+	@GetMapping("/restaurateur/produit/update/{id}")
+	public ModelAndView afficherUpdateForm(ModelAndView mv) {
+		mv.setViewName("produit/updateForm");
+		return mv;
 	}
-	
-	@PostMapping("/produit/restaurateur/update/{id}")
-	public ModelAndView updateForm(ModelAndView mv) {
-		mv.setViewName("/produit/updateForm");
-	return mv;
+
+	@PostMapping("/restaurateur/produit/update/{id}")
+	public String traiterUpdateForm() {
+		return "redirect:/produit";
 	}
-	
-	@PostMapping("/produit/restaurateur/delete/{id}")
-	public ModelAndView treatDeleteForm(ModelAndView mv) {
-		mv.setViewName("/produit/deleteForm");
-	return mv;
+
+	@PostMapping("/restaurateur/produit/delete/{id}")
+	public String traiterDeleteForm() {
+		return "redirect:/produit";
 	}
-	
+
+	@GetMapping("/user/produit/{id}")
+	public ModelAndView showProduit(ModelAndView mv) {
+		mv.setViewName("produit/show");
+		return mv;
+	}
+
+	@GetMapping("/user/produit/search")
+	public ModelAndView searchProduit(ModelAndView mv,
+											@RequestParam(name = "name") String nameProduit) {
+		mv.addObject("nameProduit", nameProduit);
+		mv.setViewName("produit/afficherResultat");
+		return mv;
+	}
 
 }
