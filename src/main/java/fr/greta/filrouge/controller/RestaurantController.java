5a673de0be809e5a12dbde1d4af8abab72f7cc25
LuisaGreta <luisafrv@gmail.com>
2020-07-30 11:05:34 +0200

    creation Categorie

--- src/main/java/fr/greta/filrouge/controller/RestaurantController.java
+++ src/main/java/fr/greta/filrouge/controller/RestaurantController.java
@@ -1,14 +1,21 @@
 package fr.greta.filrouge.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+//@RequestMapping("/user")
 public class RestaurantController {
-	@GetMapping("/")
-	public ModelAndView showAll(ModelAndView mv) {
-		mv.setViewName("/index");
-	return mv;
+	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mv) {
+		mv.setViewName("index");
+		mv.addObject("idCategorie", "15§");
+		mv.addObject("nomCategorie");
+		mv.addObject("message", "Bienvenue chez nous !");
+		return mv;
+//		return new ModelAndView("index", "message", "Hello World !");
 	}
 }
+
---
+++ src/main/java/fr/greta/filrouge/model/Categorie.java
@@ -0,0 +1,94 @@
+package fr.greta.filrouge.model;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+import fr.greta.filrouge.model.Produit;
+
+@Entity
+
+public class Categorie {
+@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String nom;
+	
+	@ManyToOne
+	private Categorie categorieParente;
+	
+	@ManyToMany
+	private List<Produit> produits;
+	
+	@Lob
+	private byte[] image;
+	
+	@Transient
+	private String image64;
+	
+	public String getImage64() {
+		return image64;
+	}
+
+
+	public void setImage64(String image64) {
+		this.image64 = image64;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Categorie getCategorieParente() {
+		return categorieParente;
+	}
+
+
+	public void setCategorieParente(Categorie categorieParente) {
+		this.categorieParente = categorieParente;
+	}
+
+
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+
+
+	public byte[] getImage() {
+		return image;
+	}
+
+
+	public void setImage(byte[] image) {
+		this.image = image;
+	}
+}
