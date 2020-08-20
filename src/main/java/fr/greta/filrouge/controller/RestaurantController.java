558fb7d76a792a2400d42ce4eec303a452f9551e
jomenoube60 <jo.menoube@gmail.com>
2020-08-20 16:35:13 +0200

    update Restaurant Controlleur

--- src/main/java/fr/greta/filrouge/controller/RestaurantController.java
+++ src/main/java/fr/greta/filrouge/controller/RestaurantController.java
@@ -1,21 +1,39 @@
 package fr.greta.filrouge.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+import java.util.Optional;
+
+import fr.greta.filrouge.model.Categorie;
+import fr.greta.filrouge.model.Restaurant;
+import fr.greta.filrouge.repos.CategorieRepository;
+import fr.greta.filrouge.repos.RestaurantRepository;
+
 @Controller
 //@RequestMapping("/user")
 public class RestaurantController {
+	@Autowired
+	private RestaurantRepository restoRepo;
+	private CategorieRepository categorieRepo;
+
 	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mv) {
 		mv.setViewName("index");
-		mv.addObject("idCategorie", "15§");
-		mv.addObject("nomCategorie");
-		mv.addObject("message", "Bienvenue chez nous !");
+		Restaurant resto = null ;
+		Optional <Restaurant>restoOpt = restoRepo.findById(1);
+		if (restoOpt.isPresent() ) {
+			resto = restoOpt.get();
+			mv.addObject("restaurant" , resto);
+		}
+
+		List <Categorie> categorieList = categorieRepo.findAll();
+		mv.addObject("categorie" , categorieList);
 		return mv;
 //		return new ModelAndView("index", "message", "Hello World !");
 	}
 }
-
